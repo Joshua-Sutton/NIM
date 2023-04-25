551f0ad4177b20e3942e7b6a152be1024a70f0f1
dlainez1 <115439403+dlainez1@users.noreply.github.com>
2023-04-24 21:57:33 -0500

    testing
    I did some changes to the game logic to make sure negotiations are working correctly.

--- nim.h
+++ nim.h
@@ -1,4 +1,7 @@
 #pragma once
+#include<vector>
+#include<string>
+#include<iostream>
 #define MAX_NAME 80
 #define MAX_PILES 9
 #define MIN_PILES 3
@@ -12,10 +15,173 @@
 #define Nim_CHALLENGE "Player="
 const int CHALLENGER = 1;
 const int HOST = 2;
+const int noWinner = 0;
+const int hostWinner = 1;
+const int clientWinner = 2;
+const int tie = 3;
+const int ABORT = -1;
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
 struct ServerStruct {
 	char name[MAX_NAME];
 	sockaddr_in addr;
+	
 };
+
+struct Move {
+	string moveString;
+	int pileIndex;
+	int rocksToRemove;
+};
+
+class GameBoard {
+	vector<int> board;
+	int piles;
+public:
+	GameBoard() : piles{ 0 } {};
+	void setBoard(string pile_size) {
+		this->piles = pile_size[0] - '0';
+		for (int c = 1; c <= sizeof(pile_size) - 2; c += 2) {
+			board.push_back((pile_size[c] - '0') * 10 + (pile_size[c + 1] - '0'));
+		}
+	}
+
+	vector<int> setBoard() {
+		return this->board;
+	}
+
+	void updateBoard(int pile, int amountToRemove) {
+		if (this->board[pile - 1] != 0)
+			this->board[pile - 1] -= amountToRemove;
+		else std::cout << "Cannot remove from empty pile" << std::endl;
+	}
+
+	void printBoard() {
+		int row = 1;
+		std::cout << std::endl;
+		for (const auto& pile : this->board) {
+			std::cout << "Row " << row++ << ": ";
+
+			for (int rock = 0; rock < pile; ++rock) {
+				std::cout << " * ";
+			}
+			std::cout << std::endl;
+		}
+
+		std::cout << std::endl;
+	}
+
+
+	int getPiles() {
+		return this->piles;
+	}
+
+	Move getMove(const int player) {
+		int move;
+		string inputOne;
+		string move_str;
+		string comment = "";
+		int pile;
+		int rocksToRemove;
+		char newline;
+		Move returnMove;
+		inputOne = "invalid";
+		while (inputOne == "invalid") {
+			std::cout << "Please input a number 1-" << this->getPiles() << " to select a pile to remove from, The letter 'C' to send a comment, or the letter 'F' to forfeit" << std::endl;
+			std::cin >> inputOne;
+			std::cin.get(newline);
+
+			//Handle a Comment
+			if (inputOne[0] == 'C' || inputOne[0] == 'c') {
+				std::cout << "Please type the comment you would like to send. ";
+				std::getline(std::cin, comment);
+				while (comment.length() > 80) {
+					std::cout << "Your comment was too long, please send something shorter. ";
+					std::getline(std::cin, comment);
+				}
+				returnMove.moveString = 'C' + comment;
+				return returnMove;
+			}
+
+			//Handle a Forfiet
+			else if (inputOne[0] == 'F' || inputOne[0] == 'f') {
+				returnMove.moveString = "F";
+				return returnMove;
+			}
+
+			//Get a number of Piles
+			else if ((inputOne[0] - '0') >= 1 && (inputOne[0] - '0') <= this->getPiles()) {
+				std::string rocks = "";
+				char newline;
+				pile = inputOne[0] - '0';
+				if (this->setBoard()[pile - 1] <= 0) {
+					std::cout << "This pile has no rocks! Please chose a different pile" << std::endl;
+					inputOne = "invalid";
+				}
+				else {
+					std::cout << "Now enter how many rocks to remove ";
+					std::cin >> rocks;
+					if (rocks[1] == NULL) {
+						rocks = '0' + rocks;
+					};
+					std::cin.get(newline);
+					rocksToRemove = (rocks[0] - '0') * 10 + (rocks[1] - '0');
+					while (this->setBoard()[pile - 1] < rocksToRemove || rocksToRemove <= 0) {
+						std::cout << "Invalid number of rocks. Please enter a number between 1 and " << this->setBoard()[pile - 1] << std::endl;
+						std::cin >> rocks;
+						std::cin.get(newline);
+						if (rocks[1] == NULL) {
+							rocks = '0' + rocks;
+						};
+						rocksToRemove = (rocks[0] - '0') * 10 + (rocks[1] - '0');
+						std::cout << rocksToRemove;
+					}
+					inputOne += rocks;
+					return Move{ inputOne, pile, rocksToRemove };
+				}
+			}
+			else {
+				std::cout << "Your input was invalid. Please try again, input a number 1-" << this->getPiles() << std::endl;
+				inputOne = "invalid";
+			}
+		}
+	}
+
+	void updateBoard(std::string move) {
+		int pile = (move[0] - '0');
+		int amountToRemove = (move[1] - '0') * 10 + (move[2] - '0');
+		this->updateBoard(pile, amountToRemove);
+	}
+
+	//This function needs to be ran when we get there move to know if they won and after our move to know if we won
+	int check4win(int player, int oponent, int myMove) {
+		//std::cout << "checking for win" << std::endl;
+		/* Need to work here */
+		int total = 0;
+		for (std::size_t i = 0; i < this->board.size(); i++) {
+			total = total + this->board[i];
+		}
+
+		if (total == 0) {
+			//std::cout << "We have a winner!!!" << std::endl;
+			if (myMove) {
+				return player;
+			}
+			else {
+				return oponent;
+			}
+		}
+
+		return noWinner;
+	}
+};
+
+
 int wait(SOCKET s, int seconds, int msec);
 sockaddr_in GetBroadcastAddress(char* IPAddress, char* subnetMask);
 void getServers(SOCKET s, sockaddr_in broadcastAddress, ServerStruct server[], int& numServers);
--- playNim.cpp
+++ playNim.cpp
@@ -1,3 +1,47 @@
+//#include <iostream>
+//#include <WinSock2.h>
+//#include <ws2tcpip.h>
+//#include <iphlpapi.h>
+//#include "nim.h"
+//#include <vector>
+//#include <string>
+//#pragma comment (lib, "iphlpapi.lib")
+//#pragma comment(lib, "Ws2_32.lib")
+//#define DEFAULT_BUFLEN 512
+//using std::vector;
+//using std::cout;
+//using std::cin;
+//using std::endl;
+//using std::string;
+//
+//int playNim(int playNim(SOCKET s, char* serverName, sockaddr_in addr, int player))
+//{
+//	int winner = noWinner;;
+//	int opponent;
+//	GameBoard board;
+//	Move move;
+//	bool playerMove;
+//	bool boardIsSet;
+//	string boardParams = "";
+//	char newline
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
 #include <WinSock2.h>
 #include <ws2tcpip.h>
@@ -24,176 +68,307 @@ int playNim(SOCKET s, char* serverName, sockaddr_in addr, int player) {
     char setNumPiles[10];
     char setNumRocks[20];
 
+   	int winner = noWinner;;
+    int opponent;
+    GameBoard board;
+    Move move;
+    bool playerMove;
+    bool boardIsSet;
+    string boardParams = "";
+    char newline;
+
     if (player == HOST) {
-        cout << "Enter the number of piles:";
+        cout << "Enter the number of piles (3-9) you want to play with:";
         cin >> numPiles;
         _itoa_s(numPiles, setNumPiles, sizeof(setNumPiles), 10);
-    }
 
-    vector<int> pile_sizes(numPiles);
-    for (int i = 0; i < numPiles; ++i) {
-        cout << "Enter the number of rocks for pile " << i + 1 << ": ";
-        cin >> pile_sizes[i];
-        _itoa_s(pile_sizes[i], setNumRocks, sizeof(setNumRocks), 10);
+        boardParams += numPiles;
 
-        //pile_sizes[i] = setNumRocks;
-    }
+        vector<int> pile_sizes(numPiles);
+        for (int i = 0; i < numPiles; ++i) {
+            cout << "Enter the number of rocks for pile " << i + 1 << ": ";
+            cin >> pile_sizes[i];
+            _itoa_s(pile_sizes[i], setNumRocks, sizeof(setNumRocks), 10);
 
-    // Start the game
-    int piles[MAX_PILES];
-    numRocks = atoi(setNumRocks);
-    for (int i = 0; i < numPiles; i++) {
-        piles[i] = numRocks;
-    }
-    player = 1;
-    while (true) {
-        // Display the current state of the game
-        cout << "Current State:" << endl;
-        for (int i = 0; i < numPiles; i++) {
-            cout << "Pile " << i + 1 << ": ";
-            for (int j = 0; j < pile_sizes[i]; j++) {
-                cout << "*";
-            }
-            cout << endl;
+            boardParams += pile_sizes[i];
         }
-        cout << endl;
 
-    }
+        cin.get(newline);
+        board.setBoard(boardParams);
+        opponent = CHALLENGER;
+        playerMove = false;
+        boardIsSet = true;
+        board.printBoard();
 
-    char sendbuf[DEFAULT_BUFLEN];
+        char sendbuf[DEFAULT_BUFLEN];
 
-    strcpy_s(sendbuf, setNumPiles);
-    strcat_s(sendbuf, setNumRocks);
+       strcpy_s(sendbuf, setNumPiles);
+       strcat_s(sendbuf, setNumRocks);
 
+       sendto(s, sendbuf, (int)strlen(sendbuf) + 1, 0, (sockaddr*)&addr, addrSize);
 
-    //Now I need to send these parameters to the client
-    // sendto(s, ?, (int)strlen(?) + 1, 0, (sockaddr*)&addr, addrSize);
-    sendto(s, sendbuf, (int)strlen(sendbuf) + 1, 0, (sockaddr*)&addr, addrSize);
-
-
-
-    //Get the player's move
-    cout << "Player " << player << "'s turn. Enter move (pile,RockCount): ";
-    int pileIndex, rocksToRemove;
-   
-
-    cin >> pileIndex >> rocksToRemove;
+       
 
 
-    // Update the game state
-    pile_sizes[pileIndex - 1] -= rocksToRemove;
+        //// Start the game
+        //int piles[MAX_PILES];
+        //numRocks = atoi(setNumRocks);
+        //for (int i = 0; i < numPiles; i++) {
+        //    piles[i] = numRocks;
+        //}
+        //player = 1;
+        ////while (true) {
+        //    // Display the current state of the game
+        //    cout << "Current State:" << endl;
+        //    for (int i = 0; i < numPiles; i++) {
+        //        cout << "Pile " << i + 1 << ": ";
+        //        for (int j = 0; j < pile_sizes[i]; j++) {
+        //            cout << "*";
+        //        }
+        //        cout << endl;
+        //    }
+        //    cout << endl;
 
-    // Check for a winner
-    bool isGameOver = true;
-    for (int i = 0; i < numPiles; i++) {
-        if (pile_sizes[i] > 0) {
-            isGameOver = false;
-            break;
-        }
-    }
-    if (isGameOver) {
-        cout << "Player " << player << " wins!" << endl;
-        //break;
-    }
-
-    // Now I need to send the move to the other player
-   // sendto(s, ?, (int)strlen(?) + 1, 0, (sockaddr*)&addr, addrSize);
-
-    char move[DEFAULT_BUFLEN];
-    char setPileIndex[sizeof(numPiles)];
-    char setRocksToRemove[sizeof(pile_sizes)];
-
-    _itoa_s(pileIndex, setPileIndex, sizeof(setPileIndex), 10);
-    _itoa_s(rocksToRemove, setRocksToRemove, sizeof(setRocksToRemove), 10);
-
-    strcpy_s(move, setPileIndex);
-    strcat_s(move, setRocksToRemove);
-    //sprintf_s(move, "%d %d", pileIndex, rocksToRemove);
-    sendto(s, move, (int)strlen(move) + 1, 0, (sockaddr*)&addr, addrSize);
-    return 0;
-
-    // Switch to the other player
-    player = (player == 1) ? 2 : 1;
-
-   
-
-
-}
-
-//playGame(s);
         
+
+        //char sendbuf[DEFAULT_BUFLEN];
+
+        //strcpy_s(sendbuf, setNumPiles);
+        //strcat_s(sendbuf, setNumRocks);
+
+
+        ////Now I need to send these parameters to the client
+        //// sendto(s, ?, (int)strlen(?) + 1, 0, (sockaddr*)&addr, addrSize);
+        //sendto(s, sendbuf, (int)strlen(sendbuf) + 1, 0, (sockaddr*)&addr, addrSize);
+
+
+
+        //Get the player's move
+       // cout << "Player " << player << "'s turn. Enter move (pile,RockCount): ";
+       // int pileIndex, rocksToRemove;
+
+
+       // cin >> pileIndex >> rocksToRemove;
+
+
+       // // Update the game state
+       // pile_sizes[pileIndex - 1] -= rocksToRemove;
+
+       // // Check for a winner
+       // bool isGameOver = true;
+       // for (int i = 0; i < numPiles; i++) {
+       //     if (pile_sizes[i] > 0) {
+       //         isGameOver = false;
+       //         break;
+       //     }
+       // }
+       // if (isGameOver) {
+       //     cout << "Player " << player << " wins!" << endl;
+       //     //break;
+       // }
+
+       // // Now I need to send the move to the other player
+       //// sendto(s, ?, (int)strlen(?) + 1, 0, (sockaddr*)&addr, addrSize);
+
+       // char move[DEFAULT_BUFLEN];
+       // char setPileIndex[sizeof(numPiles)];
+       // char setRocksToRemove[sizeof(pile_sizes)];
+
+       // _itoa_s(pileIndex, setPileIndex, sizeof(setPileIndex), 10);
+       // _itoa_s(rocksToRemove, setRocksToRemove, sizeof(setRocksToRemove), 10);
+
+       // strcpy_s(move, setPileIndex);
+       // strcat_s(move, setRocksToRemove);
+       // sprintf_s(move, "%d %d", pileIndex, rocksToRemove);
+       // sendto(s, move, (int)strlen(move) + 1, 0, (sockaddr*)&addr, addrSize);
+       // return 0;
+
+       // // Switch to the other player
+       // player = (player == 1) ? 2 : 1;
+
+
+    }
+    else {
+
         //CLIENT SIDE
-        
-        void playGame(SOCKET s, char* serverName, sockaddr_in addr, int player) {
 
-            int addrSize = sizeof(addr);
+        opponent = HOST;
+        boardIsSet = false;
+        playerMove = true;
+    }
 
-            //It needs to receive the parameters from the server
-            char gameParams[DEFAULT_BUFLEN];
-            recv(s, gameParams, sizeof(gameParams), 0);
-            int numPiles;//  stonesPerPile
-            sscanf_s(gameParams, "%d", &numPiles);
-            vector<int> pile_sizes(numPiles);
-            sscanf_s(gameParams, "%d", &pile_sizes);
-            //sscanf_s(gameParams, "%d %d", &numPiles, &pile_sizes);
-            
-            // Start the game
-            player = 2;
-            while (true) {
-                // Receive the current state of the game from the server
-                char stateBuf[1024];
-                recvfrom(s, stateBuf, (int)strlen(stateBuf) + 1, 0, (sockaddr*)&addr, &addrSize);
-                //recv(s, stateBuf, sizeof(stateBuf), 0);
-                //stateBuf[numPiles * pile_sizes] = '\0';
-                cout << "Current State:" << endl;
-                for (int i = 0; i < numPiles; i++) {
-                    cout << "Pile " << i + 1 << ": ";
-                    for (int j = 0; j < pile_sizes[i]; j++) {
-                        if (stateBuf[i * pile_sizes[i] + j] == '*') {
-                            cout << "*";
-                        }
-         else {
-          cout << " ";
-        }
-        }
-        cout << endl;
-        }
-        cout << endl;
-        
-        // Check for a winner
-        bool isGameOver = true;
-        for (int i = 0; i < numPiles; i++) {
-            for (int j = 0; j < pile_sizes[i]; j++) {
-                if (stateBuf[i * pile_sizes[i] + j] == '*') {
-                    isGameOver = false;
-                    break;
+    while (winner==noWinner)
+    {
+        if (boardIsSet) {
+            if (playerMove) {
+                move = board.getMove(player);
+                while (move.moveString[0] == 'C') {
+                    sendto(s, (char*)move.moveString.c_str(), strlen(move.moveString.c_str()) + 1, 0, (sockaddr*)&addr, addrSize);
+                    move = board.getMove(player);
+                }
+                if (move.moveString[0] == 'F') {
+                    sendto(s, (char*)move.moveString.c_str(), strlen(move.moveString.c_str()) + 1, 0, (sockaddr*)&addr, addrSize);
+                    winner = opponent;
+                    std::cout << "You forfeited!!" << std::endl;
+                }
+                else {
+                    std::cout << "Board after your move:" << std::endl;
+                    board.updateBoard(move.pileIndex, move.rocksToRemove);
+                    board.printBoard();
+                    sendto(s, (char*)move.moveString.c_str(), strlen(move.moveString.c_str()) + 1, 0, (sockaddr*)&addr, addrSize);
                 }
             }
-            if (!isGameOver) {
-                break;
+            else {
+                cout << "Waiting for your opponent..." << endl << endl;
+                //Get opponent's move & display board
+                int status = wait(s, 120, 0);
+                if (status > 0) {
+                    char movecstr[80];
+                    
+                    recvfrom(s, movecstr, (int)strlen(movecstr) + 1, 0, (sockaddr*)&addr, &addrSize);
+                    
+                    if (movecstr[0] == 'C') {
+                        std::cout << "Comment from your opponent: ";
+
+                        //declare iterator to point at the beginning of the array
+                        const char* it = std::begin(movecstr);
+                        //skip first character
+                        ++it;
+
+                        //iterate while (*it) is truthy
+                        for (; *it; ++it) std::cout << *it;
+                        std::cout << std::endl;
+
+                        //not my turn yet, as it was just a comment what we received.
+                        playerMove = !playerMove;
+                    }
+                    else if (movecstr[0] == 'F' || movecstr[0] == 'f') {
+                        std::cout << "Your opponent forfeited!" << std::endl;
+                        winner = player;
+                    }
+                    else {
+                        board.updateBoard(movecstr);
+                        std::cout << "Current state of the game:" << std::endl;
+                        board.printBoard();
+                    }
+                }
+                else {
+                    winner = ABORT;
+                }
             }
         }
-        if (isGameOver) {
-            cout << "Player " << player << " wins!" << endl;
-            break;
-        }
-        
-        // Get the player's move
-        cout << "Player " << player << "'s turn. Enter move (pile, RockCount): ";
-        int pileIndex, rocksToRemove;
-        cin >> pileIndex >> rocksToRemove;
-        
-        // Send the move to the server
-        char moveBuf[4];
-        moveBuf[0]= pileIndex + '0';
-        moveBuf[1] = ' ';
-        moveBuf[2] = rocksToRemove + '0';
-        moveBuf[3] = '\0';
-        send(s, moveBuf, sizeof(moveBuf), 0);
-        
-        // Switch players
-        player = 3 - player;
+        else {
+            cout << "Waiting for board parameters..." << std::endl << std::endl;
+            int status = wait(s, 120, 0);
+            if (status > 0) {
+                playerMove = false;
+                boardIsSet = true;
+                char boardConf[80];
+                recvfrom(s, boardConf, (int)strlen(boardConf) + 1, 0, (sockaddr*)&addr, &addrSize);
+              
+                board.setBoard(boardConf);
+                board.printBoard();
+            }
+            else {
+                winner = ABORT;
             }
         }
+
+        if (winner == ABORT) {
+            std::cout << "No response from opponent.  Aborting the game..." <<endl;
+        }
+        else {
+            if (winner == noWinner) winner = board.check4win(player, opponent, playerMove);
+        }
+
+        if (winner == player)
+            std::cout << "You WIN!" << std::endl;
+        else if (winner == opponent)
+            std::cout << "I'm sorry.  You lost" << std::endl;
+
+
+        playerMove = !playerMove;
+
+    }
+
+    return winner;
+            //It needs to receive the parameters from the server
+            //char gameParams[DEFAULT_BUFLEN];
+            //recv(s, gameParams, sizeof(gameParams), 0);
+            //int numPiles;//  stonesPerPile
+            //sscanf_s(gameParams, "%d", &numPiles);
+            //vector<int> pile_sizes(numPiles);
+            //sscanf_s(gameParams, "%d", &pile_sizes);
+            ////sscanf_s(gameParams, "%d %d", &numPiles, &pile_sizes);
+
+            //// Start the game
+            //player = 2;
+            //while (true) {
+            //    // Receive the current state of the game from the server
+            //    char stateBuf[1024];
+            //    recvfrom(s, stateBuf, (int)strlen(stateBuf) + 1, 0, (sockaddr*)&addr, &addrSize);
+            //    //recv(s, stateBuf, sizeof(stateBuf), 0);
+            //    //stateBuf[numPiles * pile_sizes] = '\0';
+            //    cout << "Current State:" << endl;
+            //    for (int i = 0; i < numPiles; i++) {
+            //        cout << "Pile " << i + 1 << ": ";
+            //        for (int j = 0; j < pile_sizes[i]; j++) {
+            //            if (stateBuf[i * pile_sizes[i] + j] == '*') {
+            //                cout << "*";
+            //            }
+            //            else {
+            //                cout << " ";
+            //            }
+            //        }
+            //        cout << endl;
+            //    }
+            //    cout << endl;
+
+            //    // Check for a winner
+            //    bool isGameOver = true;
+            //    for (int i = 0; i < numPiles; i++) {
+            //        for (int j = 0; j < pile_sizes[i]; j++) {
+            //            if (stateBuf[i * pile_sizes[i] + j] == '*') {
+            //                isGameOver = false;
+            //                break;
+            //            }
+            //        }
+            //        if (!isGameOver) {
+            //            break;
+            //        }
+            //    }
+            //    if (isGameOver) {
+            //        cout << "Player " << player << " wins!" << endl;
+            //        break;
+            //    }
+
+            //    // Get the player's move
+            //    cout << "Player " << player << "'s turn. Enter move (pile, RockCount): ";
+            //    int pileIndex, rocksToRemove;
+            //    cin >> pileIndex >> rocksToRemove;
+
+            //    // Send the move to the server
+            //    char moveBuf[4];
+            //    moveBuf[0] = pileIndex + '0';
+            //    moveBuf[1] = ' ';
+            //    moveBuf[2] = rocksToRemove + '0';
+            //    moveBuf[3] = '\0';
+            //    send(s, moveBuf, sizeof(moveBuf), 0);
+
+            //    // Switch players
+            //    player = 3 - player;
+            //}
+        
+    }
+    
+
+
+
+    
+
+
+
+        
+        
             
     
